7033b4928985ecb1ccd26b5a38bcac62af7635b6
snapmac <mohamdib@gmail.com>
2018-05-28 17:17:16 +0300

    push

--- app/src/main/java/tech/nadlan/com/nadlanproject/MainActivity.java
+++ app/src/main/java/tech/nadlan/com/nadlanproject/MainActivity.java
@@ -12,11 +12,19 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Calendar;
 
@@ -24,8 +32,15 @@ import in.shadowfax.proswipebutton.ProSwipeButton;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth;
     TextView userNameTv;
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +49,31 @@ public class MainActivity extends AppCompatActivity {
         userNameTv = (TextView)findViewById(R.id.userNameTv);
         setUserName();
         proSwipeBtn.setText("החלק כדי להיכנס");
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.createUserWithEmailAndPassword("abdalhadi6@gmail.com", "123456")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("fb-mauth", "createUserWithEmail:success "+ mAuth.getCurrentUser());
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.d("fb-mauth", "createUserWithEmail:failure1", task.getException());
+
+                        }
+
+                        // ...
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("fb-mauth", "createUserWithEmail:failure2"+  e.getMessage().toString());
+
+            }
+        });
         proSwipeBtn.showResultIcon(false);
         setTimeMessage();
         proSwipeBtn.setOnSwipeListener(new ProSwipeButton.OnSwipeListener() {
